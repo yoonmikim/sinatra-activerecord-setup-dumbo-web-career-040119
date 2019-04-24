b8ba834844e0160acd9f2bcbf4d576a285b3b601
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-24 14:30:16 +0000

    Automatically backed up by Learn

--- db/migrate/20190424141455_create_dogs.rb
+++ db/migrate/20190424141455_create_dogs.rb
@@ -1,8 +1,19 @@
 class CreateDogs < ActiveRecord::Migration
+<<<<<<< HEAD
   def change
+=======
+  def up
+>>>>>>> 7194b36252aa42c0b024c76fbe5706eda17a7e19
     create_table :dogs do |t|
       t.string :name
       t.string :breed
     end
   end
+<<<<<<< HEAD
+=======
+ 
+  def down
+    drop_table :dogs
+  end
+>>>>>>> 7194b36252aa42c0b024c76fbe5706eda17a7e19
 end
